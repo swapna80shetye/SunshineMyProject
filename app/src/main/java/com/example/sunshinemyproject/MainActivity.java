cf76dacdefd551b525cfd9c6e3de334d3a3ba45e
Swapna <sw@frisq.se>
2019-10-06 19:09:26 +0200

    Created a menu created a string for the search Item in the menu- search created main.xml that defines attributes for the menu item added methods to mainactivity.java that overrides some functionalities

--- app/src/main/java/com/example/sunshinemyproject/MainActivity.java
+++ app/src/main/java/com/example/sunshinemyproject/MainActivity.java
@@ -1,22 +1,50 @@
 package com.example.sunshinemyproject;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView myTexts;
-    private EditText myE;
-          // private TextView weatherTexts;
+    private EditText searchURLText;
+    private TextView displaySearchURL;
+    private TextView displaySearchResult;
+
+
+    // private TextView weatherTexts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myE = (EditText) findViewById(R.id.url_edit_text);
-        myTexts=findViewById(R.id.url_text);
-        //weatherTexts = (TextView) findViewById(R.id.weather_List);
+        searchURLText = (EditText) findViewById(R.id.url_search_text);
+        displaySearchURL = (TextView) findViewById(R.id.display_text);
+        displaySearchResult = (TextView) findViewById(R.id.url_result_text);
 
-        }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        int menuItemThatwasSelected = item.getItemId();
+        if(menuItemThatwasSelected == R.id.action_search){
+            Context context = MainActivity.this;
+            String textToShow = "Search clicked";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
